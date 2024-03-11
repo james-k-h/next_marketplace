24edab9ca230902dea9c35c6b779eab8fdb363b1
James <jkhallpurpose@gmail.com>
2024-03-10 20:20:23 -0400

    2

---
+++ src/components/Icons.tsx
@@ -0,0 +1,32 @@
+import { LucideProps } from 'lucide-react';
+
+export const Icons = {
+  logo: (props: LucideProps) => (
+    <svg {...props} viewBox="0 0 24 24">
+      <g>
+        <path
+          d="m2 7 4.41-4.41A2 2 0 0 1 7.83 2h8.34a2 2 0 0 1 1.42.59L22 7"
+          fill="#0c0a0a"
+          opacity="1"
+        ></path>
+        <path
+          d="M4 12v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8"
+          fill="#0c0a0a"
+          opacity="1"
+        ></path>
+        <path
+          d="M15 22v-4a2 2 0 0 0-2-2h-2a2 2 0 0 0-2 2v4"
+          fill="#e75555"
+          opacity="1"
+        ></path>
+        <path d="M2 7h20" fill="#e75555" opacity="1"></path>
+        <path
+          d="M22 7v3a2 2 0 0 1-2 2v0a2.7 2.7 0 0 1-1.59-.63.7.7 0 0 0-.82 0A2.7 2.7 0 0 1 16 12a2.7 2.7 0 0 1-1.59-.63.7.7 0 0 0-.82 0A2.7 2.7 0 0 1 12 12a2.7 2.7 0 0 1-1.59-.63.7.7 0 0 0-.82 0A2.7 2.7 0 0 1 8 12a2.7 2.7 0 0 1-1.59-.63.7.7 0 0 0-.82 0A2.7 2.7 0 0 1 4 12v0a2 2 0 0 1-2-2V7"
+          fill="#0c0b0b"
+          data-original="#464d59"
+          opacity="1"
+        ></path>
+      </g>
+    </svg>
+  ),
+};
