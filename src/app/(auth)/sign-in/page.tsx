62468757d12205e563a394283ffc2c6a75190615
James <jkhallpurpose@gmail.com>
2024-03-24 21:47:58 -0400

    new

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -5,7 +5,7 @@ import { Button, buttonVariants } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { cn } from '@/lib/utils';
 import { Label } from '@radix-ui/react-label';
-import { ArrowRight } from 'lucide-react';
+import { ArrowRight, Loader2 } from 'lucide-react';
 import Link from 'next/link';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
@@ -16,9 +16,22 @@ import {
 import { trpc } from '@/trpc/client';
 import { toast } from 'sonner';
 import { ZodError } from 'zod';
-import { useRouter } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
 
 const Page = () => {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const isSeller = searchParams.get('as') === 'seller';
+  const origin = searchParams.get('origin');
+
+  const continueAsSeller = () => {
+    router.push('?as=seller');
+  };
+
+  const continueAsBuyer = () => {
+    router.replace('/sign-in', undefined);
+  };
+
   const {
     register,
     handleSubmit,
@@ -27,46 +40,53 @@ const Page = () => {
     resolver: zodResolver(AuthCredentialsValidator),
   });
 
-  const router = useRouter();
+  const { mutate: signIn, isLoading } = trpc.auth.signIn.useMutation({
+    onSuccess: async () => {
+      toast.success('Signed in successfully');
 
-  const { mutate, isLoading } = trpc.auth.createPayloadUser.useMutation({
-    onError: (err) => {
-      if (err.data?.code === 'CONFLICT') {
-        toast.error('This email is already in use. Sign in instead.');
+      router.refresh();
+
+      if (origin) {
+        router.push(`/${origin}`);
         return;
       }
-      if (err instanceof ZodError) {
-        toast.error(err.issues[0].message);
+
+      if (isSeller) {
+        router.push('/sell');
         return;
       }
-      toast.error('Something went wrong. Please try again');
+
+      router.push('/');
     },
-    onSuccess: ({ sentToEmail }) => {
-      toast.success(`Verification email sent to ${sentToEmail}.`);
-      router.push('/verify-email?to=' + sentToEmail);
+    onError: (err) => {
+      if (err.data?.code === 'UNAUTHORIZED') {
+        toast.error('Invalid email or password.');
+      }
     },
   });
-  // const { data } = trpc.anyApiRoute.useQuery();
-  // console.log(data);
+
   const onSubmit = ({ email, password }: TAuthCredentialsValidator) => {
-    mutate({ email, password });
+    signIn({ email, password });
   };
 
   return (
     <>
       <div className="container relative flex pt-20 flex-col items-center justify-center lg:px-0">
-        <div className="mx-auto w-full flex-col justify-center space-y-6 sm:w-[350px]">
+        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
           <div className="flex flex-col items-center space-y-2 text-center">
             <Icons.logo className="h-20 w-20" />
-            <h1 className="text-2xl font-bold">Create an Account</h1>
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Sign in to your {isSeller ? 'seller' : ''} account
+            </h1>
+
             <Link
-              href="/sign-in"
               className={buttonVariants({
                 variant: 'link',
                 className: 'gap-1.5',
               })}
+              href="/sign-up"
             >
-              Already have an account? Sign-in
+              Don&apos;t have an account?
               <ArrowRight className="h-4 w-4" />
             </Link>
           </div>
@@ -89,6 +109,7 @@ const Page = () => {
                     </p>
                   )}
                 </div>
+
                 <div className="grid gap-1 py-2">
                   <Label htmlFor="password">Password</Label>
                   <Input
@@ -106,9 +127,46 @@ const Page = () => {
                   )}
                 </div>
 
-                <Button>Sign Up</Button>
+                <Button disabled={isLoading}>
+                  {isLoading && (
+                    <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                  )}
+                  Sign in
+                </Button>
               </div>
             </form>
+
+            <div className="relative">
+              <div
+                aria-hidden="true"
+                className="absolute inset-0 flex items-center"
+              >
+                <span className="w-full border-t" />
+              </div>
+              <div className="relative flex justify-center text-xs uppercase">
+                <span className="bg-background px-2 text-muted-foreground">
+                  or
+                </span>
+              </div>
+            </div>
+
+            {isSeller ? (
+              <Button
+                onClick={continueAsBuyer}
+                variant="secondary"
+                disabled={isLoading}
+              >
+                Continue as customer
+              </Button>
+            ) : (
+              <Button
+                onClick={continueAsSeller}
+                variant="secondary"
+                disabled={isLoading}
+              >
+                Continue as seller
+              </Button>
+            )}
           </div>
         </div>
       </div>
